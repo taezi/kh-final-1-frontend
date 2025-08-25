129943b1d929d6c0cef8acd249c8f314e8a37557
LeeSSSSo <thdusl0615@gmail.com>
2025-08-25 18:03:18 +0900

    add

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,9 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import "../css/EditorPage.css";
+
 export default function EditorPage(params) {
+    const navigate = useNavigate();
+  
+     const handleClick = () => {
+      // EditorWritePage로 이동
+    navigate("/editorWrite"); 
+  };
+
+  
   return (
     <Layout>
-      <h3>에디터페이지</h3>
+      <div className="editor-page">
+        <h3>에디터페이지</h3>
+        <button className="register-btn" onClick={handleClick}>
+          등록
+        </button>
+      </div>
     </Layout>
   );
 }
