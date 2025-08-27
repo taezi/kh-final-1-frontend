f7f9f1fd0b4fa76905da55e2924ff1db1e062fb0
codms-kim <codmsyaya@gmail.com>
2025-08-27 17:30:40 +0900

    비밀번호 찾기 jsx 수정

--- src/pages/FindPwdPage.jsx
+++ src/pages/FindPwdPage.jsx
@@ -1,12 +1,4 @@
-import Layout from "../components/Layout";
-import "../css/FindPwdPage.css";
-export default function FindpwdPage(params) {
-  return (
-    <Layout>
-      <h3>비밀번호 찾기 페이지</h3>
-    </Layout>
-  );
-}
+
 import { useState } from "react";
 import Layout from "../components/Layout";
 import "../css/FindPwdPage.css";
